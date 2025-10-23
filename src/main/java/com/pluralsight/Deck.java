644b3931724a3e11b472bd56f3481a90e093dfc1
lijas <lijason991@gmail.com>
2025-10-23 00:18:01 -0400

    completed preliminary Deck class

--- src/main/java/com/pluralsight/Deck.java
+++ src/main/java/com/pluralsight/Deck.java
@@ -1,4 +1,38 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Deck {
+
+    private final ArrayList<Card> cards;
+
+    public Deck() {
+        this.cards = new ArrayList<>();
+
+        String[] suits = {"Hearts", "Spades", "Diamonds", "Clubs"};
+        String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+        for (String suit : suits) {
+            for (String value: values) {
+                Card nextCard = new Card(suit, value);
+                cards.add(nextCard);
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card deal() {
+        if (!cards.isEmpty()) {
+            return cards.remove(0);
+        }
+        return null;
+    }
+
+    public int getDeckSize() {
+        return cards.size();
+    }
+
 }
