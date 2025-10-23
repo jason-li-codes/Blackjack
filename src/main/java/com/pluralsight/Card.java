cedc047e8a5347b76d54fb5fbe8db56ac93d7c3e
lijas <lijason991@gmail.com>
2025-10-23 00:17:34 -0400

    completed preliminary Card class

--- src/main/java/com/pluralsight/Card.java
+++ src/main/java/com/pluralsight/Card.java
@@ -2,6 +2,47 @@ package com.pluralsight;
 
 public class Card {
 
+    private final String suit;
+    private final String value;
+    private final String name;
+    private boolean isFaceUp;
 
+    public Card(String suit, String value) {
+        this.suit = suit;
+        this.value = value;
+        this.name = value + " of " + suit;
+        this.isFaceUp = false;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isFaceUp() {
+        return isFaceUp;
+    }
+
+    public void flip() {
+        isFaceUp = !isFaceUp;
+    }
+
+    public int getPointValue() {
+        if (isFaceUp) {
+            return switch (this.value) {
+                case "A" -> 11;
+                case "J", "Q", "K" -> 10;
+                default -> Integer.parseInt(this.value);
+            };
+        }
+        return 0;
+    }
 
 }
--- src/main/java/com/pluralsight/Player.java
+++ src/main/java/com/pluralsight/Player.java
@@ -1,4 +1,44 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Player {
+
+    private String name;
+    private ArrayList<Card> hand;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public void deal(Card card) {
+        hand.add(card);
+    }
+
+    public int getNumOfCards() {
+        return hand.size();
+    }
+
+    public int getValueOfHand() {
+
+        int totalValue = 0;
+        int aceCounter = 0;
+
+        for (Card card : hand) {
+            card.flip();
+            if (!Objects.equals(card.getValue(), "A")) {
+                totalValue += card.getPointValue();
+            } else {
+                aceCounter += 1;
+            }
+            card.flip();
+        }
+        if (totalValue + aceCounter + 10 > 21) {
+            return totalValue + aceCounter;
+        } else {
+            return totalValue + aceCounter + 10;
+        }
+    }
+
 }
